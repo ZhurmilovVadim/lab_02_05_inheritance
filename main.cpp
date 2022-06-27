9a981ab30b2b11d700ae0a289b53a84b4a355ff4
ZhurmilovVadim <vzhurmilov@gmail.com>
2022-06-28 00:51:59 +0300

    windows addition

--- main.cpp
+++ main.cpp
@@ -3,62 +3,62 @@
 #include "Set.h"
 
 int main() {
-
+    setlocale(LC_ALL, "Russian");
     vector<String> v(String("Hello!"));
-    v.push_back(String("ÐŸÑ€Ð¸Ð²ÐµÑ‚!"));
-    v.push_back(String("ÐŸÑ€Ð¸Ð²ÐµÑ‚!"));
-    v.push_back(String("ÐŸÑ€Ð¸Ð²ÐµÑ‚!"));
-    v.push_back(String("ÐŸÑ€Ð¸Ð²ÐµÑ‚!"));
-    v.push_back(String("ÐŸÑ€Ð¸Ð²ÐµÑ‚!"));
-    std::cout<<"Ð’ÐµÐºÑ‚Ð¾Ñ€ v: "<< v <<std::endl;
-    v.push_back(String("ÐŸÑ€Ð¸Ð²ÐµÑ‚!"));
-    v.push_back(String("ÐŸÑ€Ð¸Ð²ÐµÑ‚!"));
-    v.push_back(String("ÐŸÑ€Ð¸Ð²ÐµÑ‚!"));
-    std::cout<<"Ð’ÐµÐºÑ‚Ð¾Ñ€ v: "<<v<<std::endl;
+    v.push_back(String("Ïðèâåò!"));
+    v.push_back(String("Ïðèâåò!"));
+    v.push_back(String("Ïðèâåò!"));
+    v.push_back(String("Ïðèâåò!"));
+    v.push_back(String("Ïðèâåò!"));
+    std::cout<<"Âåêòîð v: "<< v <<std::endl;
+    v.push_back(String("Ïðèâåò!"));
+    v.push_back(String("Ïðèâåò!"));
+    v.push_back(String("Ïðèâåò!"));
+    std::cout<<"Âåêòîð v: "<<v<<std::endl;
     vector v1=v;
-    std::cout<<"Ð’ÐµÐºÑ‚Ð¾Ñ€ v1: "<<v1<<std::endl;
+    std::cout<<"Âåêòîð v1: "<<v1<<std::endl;
     for(int i=0;i<5;i++)
         v1.pop(0);
-    std::cout<<"Ð’ÐµÐºÑ‚Ð¾Ñ€ v1: "<<v1<<std::endl;
+    std::cout<<"Âåêòîð v1: "<<v1<<std::endl;
     Set s("Yes"),s1,s2;
-    s.push_back("ÐŸÑ€Ð¸Ð²ÐµÑ‚!");
+    s.push_back("Ïðèâåò!");
     s.push_back("No");
     char* str="Hello!";
     s.push_back(str);
-    std::cout<<"ÐœÐ½Ð¾Ð¶ÐµÑÑ‚Ð²Ð¾ s: "<<s<<std::endl;
+    std::cout<<"Ìíîæåñòâî s: "<<s<<std::endl;
     s1.push_back("Cat");
     s1.push_back("No");
-    s1.push_back("ÐŸÑ€Ð¸Ð²ÐµÑ‚!");
-    std::cout<<"ÐœÐ½Ð¾Ð¶ÐµÑÑ‚Ð²Ð¾ s1: "<<s1<<std::endl;
+    s1.push_back("Ïðèâåò!");
+    std::cout<<"Ìíîæåñòâî s1: "<<s1<<std::endl;
     s2=s1-s;
-    std::cout<<"ÐœÐ½Ð¾Ð¶ÐµÑÑ‚Ð²Ð¾ s2=s1-s: "<<s2<<std::endl;
-    std::cout<<"ÐœÐ½Ð¾Ð¶ÐµÑÑ‚Ð²Ð¾ s1: "<<s1<<std::endl;
-    std::cout<<"ÐœÐ½Ð¾Ð¶ÐµÑÑ‚Ð²Ð¾ s: "<<s<<std::endl;
+    std::cout<<"Ìíîæåñòâî s2=s1-s: "<<s2<<std::endl;
+    std::cout<<"Ìíîæåñòâî s1: "<<s1<<std::endl;
+    std::cout<<"Ìíîæåñòâî s: "<<s<<std::endl;
     s2=s-s1;
-    std::cout<<"ÐœÐ½Ð¾Ð¶ÐµÑÑ‚Ð²Ð¾ s2=s-s1: "<<s2<<std::endl;
-    std::cout<<"ÐœÐ½Ð¾Ð¶ÐµÑÑ‚Ð²Ð¾ s1: "<<s1<<std::endl;
-    std::cout<<"ÐœÐ½Ð¾Ð¶ÐµÑÑ‚Ð²Ð¾ s: "<<s<<std::endl;
+    std::cout<<"Ìíîæåñòâî s2=s-s1: "<<s2<<std::endl;
+    std::cout<<"Ìíîæåñòâî s1: "<<s1<<std::endl;
+    std::cout<<"Ìíîæåñòâî s: "<<s<<std::endl;
     s2=s1+s;
-    std::cout<<"ÐœÐ½Ð¾Ð¶ÐµÑÑ‚Ð²Ð¾ s2=s1+s: "<<s2<<std::endl;
-    std::cout<<"ÐœÐ½Ð¾Ð¶ÐµÑÑ‚Ð²Ð¾ s1: "<<s1<<std::endl;
-    std::cout<<"ÐœÐ½Ð¾Ð¶ÐµÑÑ‚Ð²Ð¾ s: "<<s<<std::endl;
+    std::cout<<"Ìíîæåñòâî s2=s1+s: "<<s2<<std::endl;
+    std::cout<<"Ìíîæåñòâî s1: "<<s1<<std::endl;
+    std::cout<<"Ìíîæåñòâî s: "<<s<<std::endl;
     s2=s1*s;
-    std::cout<<"ÐœÐ½Ð¾Ð¶ÐµÑÑ‚Ð²Ð¾ s2=s1*s: "<<s2<<std::endl;
-    std::cout<<"ÐœÐ½Ð¾Ð¶ÐµÑÑ‚Ð²Ð¾ s1: "<<s1<<std::endl;
-    std::cout<<"ÐœÐ½Ð¾Ð¶ÐµÑÑ‚Ð²Ð¾ s: "<<s<<std::endl;
+    std::cout<<"Ìíîæåñòâî s2=s1*s: "<<s2<<std::endl;
+    std::cout<<"Ìíîæåñòâî s1: "<<s1<<std::endl;
+    std::cout<<"Ìíîæåñòâî s: "<<s<<std::endl;
     Set s3=s2;
-    std::cout<<"ÐœÐ½Ð¾Ð¶ÐµÑÑ‚Ð²Ð¾ s3=s2: "<<s3<<std::endl;
+    std::cout<<"Ìíîæåñòâî s3=s2: "<<s3<<std::endl;
     if(s3==s2)
-        std::cout<<"ÐœÐ½Ð¾Ð¶ÐµÑÑ‚Ð²Ð¾ s3=s2\n";
+        std::cout<<"Ìíîæåñòâî s3=s2\n";
     else
-        std::cout<<"ÐœÐ½Ð¾Ð¶ÐµÑÑ‚Ð²Ð¾ s3!=s2\n";
+        std::cout<<"Ìíîæåñòâî s3!=s2\n";
     if(s3==s1)
-        std::cout<<"ÐœÐ½Ð¾Ð¶ÐµÑÑ‚Ð²Ð¾ s3=s1\n";
+        std::cout<<"Ìíîæåñòâî s3=s1\n";
     else
-        std::cout<<"ÐœÐ½Ð¾Ð¶ÐµÑÑ‚Ð²Ð¾ s3!=s1\n";
+        std::cout<<"Ìíîæåñòâî s3!=s1\n";
     if(s1==s3)
-        std::cout<<"ÐœÐ½Ð¾Ð¶ÐµÑÑ‚Ð²Ð¾ s1=s3\n";
+        std::cout<<"Ìíîæåñòâî s1=s3\n";
     else
-        std::cout<<"ÐœÐ½Ð¾Ð¶ÐµÑÑ‚Ð²Ð¾ s1!=s3\n";
+        std::cout<<"Ìíîæåñòâî s1!=s3\n";
     return 0;
 }
